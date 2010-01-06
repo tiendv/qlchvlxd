fa78abf674b424f8f8d980e8e23b33a6d13e77d2
vtienuit <vtienuit@a3f09300-fa11-11de-8cdf-e97ad7d0f28a>
2010-01-06 05:47:03 +0000

    Cuong Commit

--- trunk/qlchvlxd/GiaoHang.Designer.cs
+++ trunk/qlchvlxd/GiaoHang.Designer.cs
@@ -39,8 +39,6 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.labelTenKH = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
@@ -150,22 +148,6 @@
             this.labelTenKH.TabIndex = 10;
             this.labelTenKH.Text = "Tên khách hàng:";
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(27, 146);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(80, 13);
-            this.label6.TabIndex = 18;
-            this.label6.Text = "Loại giao hàng:";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(123, 143);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(159, 20);
-            this.textBox5.TabIndex = 19;
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
@@ -185,7 +167,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(27, 175);
+            this.label8.Location = new System.Drawing.Point(27, 150);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(96, 13);
             this.label8.TabIndex = 22;
@@ -196,7 +178,7 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "1"});
-            this.comboBox1.Location = new System.Drawing.Point(123, 166);
+            this.comboBox1.Location = new System.Drawing.Point(123, 147);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(50, 21);
             this.comboBox1.TabIndex = 23;
@@ -287,7 +269,7 @@
             this.label9.TabIndex = 28;
             this.label9.Text = "Tổng tiền:.......";
             // 
-            // frmGiaoHang
+            // GiaoHang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -301,8 +283,6 @@
             this.Controls.Add(this.label8);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.checkBox1);
@@ -314,7 +294,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "frmGiaoHang";
+            this.Name = "GiaoHang";
             this.Text = "frmGiaoHang";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
@@ -335,8 +315,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label labelTenKH;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Label label8;
