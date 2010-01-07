01bf82de7fedce17bf057f6eed2cd04bcc8dee7f
vtienuit <vtienuit@a3f09300-fa11-11de-8cdf-e97ad7d0f28a>
2010-01-07 19:32:50 +0000

    comit tbloaisanphamservices

---
+++ trunk/BusinessLogicLayer/tbLOAISANPHAMServices.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccessLayer;
+using BusinessEntities;
+using System.Data;
+using System.Data.OleDb;
+
+namespace BusinessLogicLayer
+{
+    class tbLOAISANPHAMServices
+    {
+        public static List<BusinessEntities.tbLOAISANPHAM> getDanhSachLoaiSanPham()
+        {
+            List<BusinessEntities.tbLOAISANPHAM> list = new List<BusinessEntities.tbLOAISANPHAM>();
+            DataAccessLayer.tbLOAISANPHAMDAO access = new tbLOAISANPHAMDAO;
+
+           list = access.getLoaiSanPham();
+            if (list == null)
+                return null;
+
+            return list;
+        }     
+    }
+}
