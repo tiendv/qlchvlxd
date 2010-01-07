c629cb8b2639ddf7a1cb36443dc09eaa23b3d59a
vtienuit <vtienuit@a3f09300-fa11-11de-8cdf-e97ad7d0f28a>
2010-01-07 18:59:54 +0000

    Table Loai San Pham

---
+++ trunk/BusinessEntities/tbLOAISANPHAM.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+// table Loại Sản Phẩm
+namespace BusinessEntities
+{
+    public class tbLOAISANPHAM
+    {
+        public int maloaisp;
+        public string tenloaisanpham;
+
+        public tbLOAISANPHAM(int maloaisp, string tenloaisanpham)
+        {
+            this.maloaisp = maloaisp;
+            this.tenloaisanpham = tenloaisanpham;
+        }
+
+        public int MALOAISP
+        {
+            get { return this.maloaisp; }
+            set
+            {
+
+                if (value == 0)
+                    throw new Exception("Mã loại sản phẩm rỗng !");
+                this.maloaisp = value;
+            }
+        }
+        public string TENLOAISP
+        {
+            get { return this.tenloaisanpham; }
+            set
+            {
+
+                if (value == "")
+                    throw new Exception("Tên loại sản phẩm rỗng !");
+                this.tenloaisanpham = value;
+            }
+        }
+    }
+
+}
