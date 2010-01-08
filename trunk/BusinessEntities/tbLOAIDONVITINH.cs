d964171aa84636140d261a3b4d928d956cf8bcd0
vtienuit <vtienuit@a3f09300-fa11-11de-8cdf-e97ad7d0f28a>
2010-01-08 00:53:55 +0000

    comit tbLoaidonvitinh

---
+++ trunk/BusinessEntities/tbLOAIDONVITINH.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessEntities
+{
+    public class tbLOAIDONVITINH
+    {
+        public int maloaidonvitinh;
+        public string tenloaidonvitinh;
+
+       // constructor
+        /*
+        public tbLOAIDONVITINH(int maloaidonvitinh, string tenloaidonvitinh)
+        {
+            this.maloaidonvitinh = maloaidonvitinh;
+            this.tenloaidonvitinh = tenloaidonvitinh;
+        }
+         */
+        public int MALOAIDONVITINH
+        {
+
+            get { return this.maloaidonvitinh; }
+            set
+            {
+
+                if (value == 0)
+                    throw new Exception("Mã loại đơn vị tính rỗng !");
+                this.maloaidonvitinh = value;
+            }
+        }
+        public string TENLOAIDONVITINH
+        {
+
+            get { return this.tenloaidonvitinh; }
+            set
+            {
+
+                if (value == "")
+                    throw new Exception("Tên loại đơn vị tính  rỗng !");
+                this.tenloaidonvitinh = value;
+            }
+        }
+        
+    }
+}
