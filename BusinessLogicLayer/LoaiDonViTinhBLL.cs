7135c0e2425761e1b9176282e78bf525a6be8124
vtienuit <vtienuit@a3f09300-fa11-11de-8cdf-e97ad7d0f28a>
2010-01-08 16:17:04 +0000

    LoaiDonViTinhBLL commit

---
+++ BusinessLogicLayer/LoaiDonViTinhBLL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccessLayer;
+using BusinessEntities;
+using System.Data;
+using System.Data.OleDb;
+
+namespace BusinessLogicLayer
+{
+    public class LoaiDonViTinhBLL
+    {
+        public  List<BusinessEntities.LoaiDonViTinhBE> getDanhSachLoaiDonViTinh()
+        {
+            List<BusinessEntities.LoaiDonViTinhBE> list = new List<BusinessEntities.LoaiDonViTinhBE>();
+            DataAccessLayer.LoaiDonViTinhBE access = new DataAccessLayer.LoaiDonViTinhBE();
+
+
+            list = access.getLoaiDonViTinh();
+
+            if (list == null)
+                return null;
+
+            return list;
+        }  
+    }
+}
