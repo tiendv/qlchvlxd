83c0edade8dde5a063c74bccbf03ee576a919937
vtienuit <vtienuit@a3f09300-fa11-11de-8cdf-e97ad7d0f28a>
2010-01-08 16:16:38 +0000

    LoaiDonViTinhBE commit

---
+++ BusinessEntities/LoaiDonViTinhBE.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessEntities
+{
+    public class LoaiDonViTinhBE
+    {
+     
+        public int maloaidonvitinh;
+        public string tendonvitinh;
+
+
+
+        public LoaiDonViTinhBE()
+        { }
+
+        public int MALOAIDONVITINH
+        {
+            get { return this.maloaidonvitinh; }
+            set
+            {
+
+                if (value == 0)
+                    throw new Exception("Mã loại đơn vị tính rỗng!");
+                this.maloaidonvitinh = value;
+            }
+        }
+        public string TENDONVITINH
+        {
+            get { return this.tendonvitinh; }
+            set
+            {
+
+                if (value == "")
+                    throw new Exception("Tên  đơn vị tính rỗng!");
+                this.tendonvitinh = value;
+            }
+        }
+       
+    }
+}
