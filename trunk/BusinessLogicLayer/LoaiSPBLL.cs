67bab35bb30d81b553c6783f80fba8d5db470bdf
vtienuit <vtienuit@a3f09300-fa11-11de-8cdf-e97ad7d0f28a>
2010-01-08 15:03:41 +0000

    Lớp BLL

---
+++ trunk/BusinessLogicLayer/LoaiSPBLL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccessLayer;
+using BusinessEntities;
+using System.Data;
+using System.Data.OleDb;
+
+namespace BusinessLogicLayer
+{
+    public class LoaiSPBLL
+    {
+
+        public static List<BusinessEntities.LoaiSPBE> getDanhSachLoaiSP()
+        {
+            List<BusinessEntities.LoaiSPBE> list = new List<BusinessEntities.LoaiSPBE>();
+            DataAccessLayer.LoaiSPDAL access = new DataAccessLayer.LoaiSPDAL();
+
+
+            list = access.getLoaiSanPham();
+
+            if (list == null)
+                return null;
+
+            return list;
+        }      
+              
+    }
+}
