2a383a77c05f323e2c284966fc77b86817aa22dc
vtienuit <vtienuit@a3f09300-fa11-11de-8cdf-e97ad7d0f28a>
2010-01-08 15:02:53 +0000

    Lớp DAL

--- DataAccessLayer/LoaiSPDAL.cs
+++ DataAccessLayer/LoaiSPDAL.cs
@@ -6,36 +6,112 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 using BusinessEntities;
+/*
+  * LoaiSPDAL
+  * 
+  * Author : tiendv
+  * Date : Jan 08 2010
+  */
 
 namespace DataAccessLayer
 {
-    class LoaiSPDAL
+    public class LoaiSPDAL
     {
-        public static List<BusinessEntities.CTHoaDonBE> getLoaiSanPham()
+        public List<BusinessEntities.LoaiSPBE> getLoaiSanPham()
         {
-            DataTable dt = SQLHelp.executeQuery("select * from cthoadon");
-            List<BusinessEntities.CTHoaDonBE> danhsachCTHD = new List<BusinessEntities.CTHoaDonBE>();
+            DataTable dt = SQLHelp.executeQuery("select * from loaisanpham");
+            List<BusinessEntities.LoaiSPBE> danhsachLoaiSanPham = new List<BusinessEntities.LoaiSPBE>();
 
             try
             {
                 foreach (DataRow row in dt.Rows)
                 {
-                    BusinessEntities.CTHoaDonBE cthd = new BusinessEntities.CTHoaDonBE();
-                    cthd.maCTHD = row["macthd"].ToString();
-                    cthd.maHD = row["mahd"].ToString();
-                    cthd.maLoaiSP = (int)row["maloaisp"];
-                    cthd.maSP = (int)row["masp"];
-                    cthd.soLuong = (int)row["soluong"];
-                    cthd.donGia = (float)row["dongia"];
+                    BusinessEntities.LoaiSPBE lsp = new BusinessEntities.LoaiSPBE();
+                    lsp.maloaisp = (int)row["maloaisp"];
+                    lsp.tenloaisp = row["tenloaisp"].ToString();
 
-                    danhsachCTHD.Add(cthd);
+                    danhsachLoaiSanPham.Add(lsp);
                 }
             }
             catch (Exception e)
             {
                 return null;
             }
-            return danhsachCTHD;
+            return danhsachLoaiSanPham;
         }
+
+        /*
+      * Lấy tên những tên sản phẩm từ bảng loại sản phẩm
+ 
+      */
+        public List<string> getAllTenLoaiSanPham()
+        {
+            List<string> tenloaisp = new List<string>();
+            string query = "SELECT  DISTINCT TENLOAISP FROM LOAISANPHAM";
+            DataTable dt = new DataTable();
+
+            dt = SQLHelp.executeQuery(query);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                string str = row["TENLOAISP"].ToString();
+                tenloaisp.Add(str);
+            }
+
+            return tenloaisp;
+        }
+
+
+
+        //Lấy mã những tên sản phẩm từ bảng loại sản phẩm
+        public List<string> getAllMaLoaiSanPham()
+        {
+            List<string> maloaisp = new List<string>();
+            string query = "SELECT  DISTINCT MALOAISP FROM LOAISANPHAM";
+            DataTable dt = new DataTable();
+
+            dt = SQLHelp.executeQuery(query);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                string str = row["MALOAISP"].ToString();
+                maloaisp.Add(str);
+            }
+
+            return maloaisp;
+        }
+        // thêm một loại sản phẩm .
+
+
+        public void insertLoaiSP(LoaiSPBE lsp)
+        {
+            string query = "INSERT INTO LOAISANPHAM VALUES ('" +lsp.maloaisp + "', N'" + lsp.tenloaisp+ "') ";
+            SQLHelp.executeNonQuery(query);
+        }
+       
+        // Xóa Loại Sản Phẩm
+        
+        
+      
+        public void deleteLoaiSP(int maloaisp)
+        {
+            string query = "DELETE FROM LOAISANPHAM WHERE MALOAISP = '" + maloaisp + "'";
+            SQLHelp.executeNonQuery(query);
+        }
+        /*
+         * Sửa Thông tin Loại Sản Phẩm
+         * 
+         * 
+         */
+        public void updateLoaiSP(LoaiSPBE loaisp)
+        {
+            string query = "UPDATE LOAISANPHAM " +
+                                "SET MALOAISP =" + loaisp.maloaisp + "'," +
+                                        "TENLOAIAP = N'" + loaisp.tenloaisp + "'," +
+                                "WHERE MALOAISP = '" + loaisp.MALOAISP + "'";
+            System.Console.Out.WriteLine(query);
+            SQLHelp.executeNonQuery(query);
+        }
+
     }
 }
