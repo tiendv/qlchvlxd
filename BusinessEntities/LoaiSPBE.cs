15c5102a2be2077a10d9989c4d75f3b0527506bd
vtienuit <vtienuit@a3f09300-fa11-11de-8cdf-e97ad7d0f28a>
2010-01-08 15:04:13 +0000

    lớp BE

--- BusinessEntities/LoaiSPBE.cs
+++ BusinessEntities/LoaiSPBE.cs
@@ -5,7 +5,36 @@ using System.Text;
 
 namespace BusinessEntities
 {
-    class LoaiSPBE
+    public class LoaiSPBE
     {
+        public LoaiSPBE()
+        {}
+        public int maloaisp;
+        public string tenloaisp;
+
+        public int MALOAISP
+        {
+            get { return this.maloaisp; }
+            set
+            {
+
+                if (value ==0)
+                    throw new Exception("Mã loại sản phẩm rỗng!");
+                this.maloaisp = value;
+            }
+        }
+
+        public string TENLOAISP
+        {
+            get { return this.tenloaisp; }
+            set
+            {
+
+                if (value == "")
+                    throw new Exception("Tên loại sản phẩm rỗng!");
+                this.tenloaisp = value;
+            }
+        }
+
     }
 }
