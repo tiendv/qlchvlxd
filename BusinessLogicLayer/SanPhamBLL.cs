3ac52e944f6cbe3063d5e131f4008a11990949e2
vtienuit <vtienuit@a3f09300-fa11-11de-8cdf-e97ad7d0f28a>
2010-01-08 15:03:41 +0000

    Lớp BLL

--- BusinessLogicLayer/SanPhamBLL.cs
+++ BusinessLogicLayer/SanPhamBLL.cs
@@ -12,39 +12,28 @@ namespace BusinessLogicLayer
     public class SanPhamBLL
     {
 
+         public SanPhamBLL()
+        { 
+        
+        }
 
 
-        public static List<BusinessEntities.SanPhamBE> getDanhSachSanPham()
-        {
-            List<BusinessEntities.SanPhamBE> list = new List<BusinessEntities.SanPhamBE>();
-            DataAccessLayer.SanPhamDAL access = new SanPhamDAL();
+         public static List<BusinessEntities.SanPhamBE> getDanhSachSanPham()
+         {
+             List<BusinessEntities.SanPhamBE> list = new List<BusinessEntities.SanPhamBE>();
+             DataAccessLayer.SanPhamDAL access = new SanPhamDAL();
+            
+             list = access.getSanPham();
 
-            //list = access.getSanPham();
+             if (list == null)
+                 return null;
 
-            if (list == null)
-                return null;
-
-            return list;
-        }     
+             return list;
+         }     
  
 
 
 
-
-        /*
-        private tbsanphamDao dao;
-        public tbsanphamServices()
-        {
-            dao = new tbsanphamDao();
-        }
-
-        public DataSet getAllSanPham()
-        {
-            if (dao == null)
-                throw new Exception("Cannot get DB");
-            return dao.getAllSanPham();
-        }
-         */
 
     }
 }
