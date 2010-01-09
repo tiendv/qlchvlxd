f7e92a8c4013d3be4cd3fe3151f0179a5eb22e9a
vtienuit <vtienuit@a3f09300-fa11-11de-8cdf-e97ad7d0f28a>
2010-01-09 09:43:58 +0000

    Thiet ke Them san pham

--- qlchvlxd/ThemSanPhamPL.cs
+++ qlchvlxd/ThemSanPhamPL.cs
@@ -6,14 +6,72 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using BusinessEntities;
+using BusinessLogicLayer;
+
 
 namespace qlchvlxd
 {
     public partial class ThemSanPhamPL : Form
     {
+
+        //lay danh sach ten cac loai đơn vị tính từ bang loai đơn vị tính
+        List<BusinessEntities.LoaiDonViTinhBE> listTenLoaiDonViTinh = new List<BusinessEntities.LoaiDonViTinhBE>();
         public ThemSanPhamPL()
         {
             InitializeComponent();
+            listTenLoaiDonViTinh = BusinessLogicLayer.LoaiDonViTinhBLL.getListTenLoaiDonViTinh();
+
         }
+
+        private void textBox5_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar)) 
+                MessageBox.Show("Tiền có kiểu số ! ");
+        }
+
+        private void textBox6_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+                MessageBox.Show("Tiền có kiểu số ! ");
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+                MessageBox.Show("Số lượng có  kiểu số ! ");
+        }
+
+        private void textBox8_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+                MessageBox.Show("Số lượng có  kiểu số ! ");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void ThemSanPhamPL_Load(object sender, EventArgs e)
+        {
+            if (listTenLoaiDonViTinh == null)
+                MessageBox.Show("Danh sách rỗng!");
+            else
+            {
+                for (int i = 0; i < listTenLoaiDonViTinh.Count; i++)
+                {
+                    //MessageBox.Show("hien");
+                    comboBox1.Items.Add(listTenLoaiDonViTinh[i].tendonvitinh);
+                }
+            }
+        }
+
+       
+
+      
+
+       
     }
 }
