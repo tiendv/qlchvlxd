12e9d2f710db85d4a4f49417b4a74636950b9f14
vtienuit <vtienuit@a3f09300-fa11-11de-8cdf-e97ad7d0f28a>
2010-01-09 09:44:50 +0000

    SanPhamBLL

--- trunk/BusinessLogicLayer/SanPhamBLL.cs
+++ trunk/BusinessLogicLayer/SanPhamBLL.cs
@@ -17,19 +17,46 @@ namespace BusinessLogicLayer
         
         }
 
-
-         public static List<BusinessEntities.SanPhamBE> getDanhSachSanPham()
+        // lay danh sach san pham
+         public  List<BusinessEntities.SanPhamBE> getDanhSachSanPham()
          {
              List<BusinessEntities.SanPhamBE> list = new List<BusinessEntities.SanPhamBE>();
              DataAccessLayer.SanPhamDAL access = new SanPhamDAL();
-            
+             //access.getSanPham();
              list = access.getSanPham();
 
              if (list == null)
                  return null;
 
              return list;
-         }     
+         }
+        // lay danh sach san pham theo ten
+         public  List<BusinessEntities.SanPhamBE> getListTenSanPham(String tenSanPham)
+         {
+             List<BusinessEntities.SanPhamBE> listTenSanPham = new List<BusinessEntities.SanPhamBE>();
+             DataAccessLayer.SanPhamDAL accessTenSanPham = new DataAccessLayer.SanPhamDAL();
+
+             listTenSanPham = accessTenSanPham.viewAllSanPhamByKindOfTenSP(tenSanPham); 
+
+             if (listTenSanPham == null)
+                 return null;
+
+             return listTenSanPham;
+         }
+        // Lay danh sach san pham theo mã sản phẩm 
+
+         public List<BusinessEntities.SanPhamBE> getListMaSanPham(int msp)
+         {
+             List<BusinessEntities.SanPhamBE> listMaSanPham = new List<BusinessEntities.SanPhamBE>();
+             DataAccessLayer.SanPhamDAL accessTenSanPham = new DataAccessLayer.SanPhamDAL();
+
+             listMaSanPham = accessTenSanPham.viewAllSanPhamByKindOfMaSP(msp);
+
+             if (listMaSanPham == null)
+                 return null;
+
+             return listMaSanPham;
+         }
  
 
 
