5862331b3c2e0da117caae8ee954f4818c1b24fe
vtienuit <vtienuit@a3f09300-fa11-11de-8cdf-e97ad7d0f28a>
2010-01-09 09:43:30 +0000

    Phan Tim Kiem san Pham

--- trunk/qlchvlxd/TimKiemSanPhamPL.cs
+++ trunk/qlchvlxd/TimKiemSanPhamPL.cs
@@ -6,15 +6,54 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using BusinessEntities;
+using BusinessLogicLayer;
+using DataAccessLayer;
+
 
 namespace qlchvlxd
 {
     public partial class TimKiemSanPhamPL : Form
     {
+
         public TimKiemSanPhamPL()
         {
             InitializeComponent();
+            // hienthi();
         }
+
+        public BusinessLogicLayer.SanPhamBLL sanphamBLL = new SanPhamBLL();
+
+
+        public void hienthi()
+        {
+            //  BusinessLogicLayer.SanPhamBLL sanphamBLL = new BusinessLogicLayer.SanPhamBLL();
+            List<SanPhamBE> listSanPham = new List<SanPhamBE>();
+            listSanPham = sanphamBLL.getDanhSachSanPham();
+            if (listSanPham == null)
+                MessageBox.Show("Chi tiết sản phẩm rỗng ");
+            else
+            {
+                int stt = 1;
+                listView1.Items.Clear();
+                foreach (SanPhamBE sp in listSanPham)
+                {
+                    sp.MASP.ToString();
+                    ListViewItem item = new ListViewItem(stt.ToString());
+                    stt++;
+                    item.SubItems.Add(sp.TENSP);
+                    item.SubItems.Add(sp.MASP.ToString());
+                    item.SubItems.Add(sp.MANCC);
+                    item.SubItems.Add(sp.SOLUONG.ToString());
+                    item.SubItems.Add(sp.GIABAN.ToString());
+                    listView1.Items.Add(item);
+                }
+            }
+
+
+
+        }
+
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -23,13 +62,109 @@ namespace qlchvlxd
 
         private void Timkiemsanpham_Load(object sender, EventArgs e)
         {
-           
+            hienthi();
+
         }
 
         private void listView1_MouseClick(object sender, MouseEventArgs e)
         {
-            SanPhamPL frmForm2 = new SanPhamPL();
-            frmForm2.Show();
+            // SanPhamPL frmForm2 = new SanPhamPL();
+            // frmForm2.Show();
         }
+       
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text.Length == 0)
+            {
+                MessageBox.Show("Bạn chưa lập nội dung tìm kiếm !");
+
+            }
+            else
+            {
+                // tim kiem theo ten san pham 
+                if (radioButton1.Checked)
+                {
+                    List<SanPhamBE> listSanPham = sanphamBLL.getListTenSanPham(textBox1.Text);
+                    int stt = 1;
+                    listView1.Items.Clear();
+                    foreach (SanPhamBE sp in listSanPham)
+                    {
+                        sp.MASP.ToString();
+                        ListViewItem item = new ListViewItem(stt.ToString());
+                        stt++;
+                        item.SubItems.Add(sp.TENSP);
+                        item.SubItems.Add(sp.MASP.ToString());
+                        item.SubItems.Add(sp.MANCC);
+                        item.SubItems.Add(sp.SOLUONG.ToString());
+                        item.SubItems.Add(sp.GIABAN.ToString());
+                        listView1.Items.Add(item);
+                    }
+
+
+
+                }
+                else
+                {
+                    // tim kiem theo ma san pham
+                    
+                    
+                        if (radioButton2.Checked)
+                        {
+                            
+
+                            List<SanPhamBE> listSanPham = sanphamBLL.getListMaSanPham(int.Parse(textBox1.Text));
+                            int stt = 1;
+                            listView1.Items.Clear();
+                            foreach (SanPhamBE sp in listSanPham)
+                            {
+                                sp.MASP.ToString();
+                                ListViewItem item = new ListViewItem(stt.ToString());
+                                stt++;
+                                item.SubItems.Add(sp.TENSP);
+                                item.SubItems.Add(sp.MASP.ToString());
+                                item.SubItems.Add(sp.MANCC);
+                                item.SubItems.Add(sp.SOLUONG.ToString());
+                                item.SubItems.Add(sp.GIABAN.ToString());
+                                listView1.Items.Add(item);
+
+                            }
+                        }
+                        else
+                            if (radioButton3.Checked)
+                            {
+
+
+                                // tim kiem theo mancc
+                                MessageBox.Show("Phan cua Linh chua dua vao ");
+                            }
+                    }
+
+                
+            }
+
+
+
+        }
+        // Kiểm tra  text box vào có phải là số không
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+
+            if (!Char.IsDigit(e.KeyChar) &&radioButton2.Checked)
+            MessageBox.Show("Mã sản phẩm là số ");
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
     }
+
+    
 }
--- trunk/qlchvlxd/TimKiemSanPhamPL.designer.cs
+++ trunk/qlchvlxd/TimKiemSanPhamPL.designer.cs
@@ -28,14 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
-            "1",
-            "a",
-            "b"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
-            "2",
-            "c",
-            "d"}, -1);
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
@@ -50,20 +42,26 @@
             this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
             this.columnHeader6 = new System.Windows.Forms.ColumnHeader();
             this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(63, 43);
+            this.textBox1.Location = new System.Drawing.Point(178, 44);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(305, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(162, 18);
+            this.label1.Location = new System.Drawing.Point(295, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(119, 13);
             this.label1.TabIndex = 1;
@@ -71,17 +69,18 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(390, 43);
+            this.button1.Location = new System.Drawing.Point(532, 41);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(80, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Tìm Kiếm";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(63, 79);
+            this.radioButton1.Location = new System.Drawing.Point(45, 15);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(96, 17);
             this.radioButton1.TabIndex = 3;
@@ -92,7 +91,7 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(165, 79);
+            this.radioButton2.Location = new System.Drawing.Point(147, 15);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(92, 17);
             this.radioButton2.TabIndex = 4;
@@ -103,7 +102,7 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(283, 79);
+            this.radioButton3.Location = new System.Drawing.Point(271, 15);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(95, 17);
             this.radioButton3.TabIndex = 5;
@@ -121,13 +120,10 @@
             this.columnHeader5,
             this.columnHeader6});
             this.listView1.GridLines = true;
-            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1,
-            listViewItem2});
-            this.listView1.Location = new System.Drawing.Point(47, 114);
+            this.listView1.Location = new System.Drawing.Point(34, 141);
             this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(404, 150);
+            this.listView1.Size = new System.Drawing.Size(676, 218);
             this.listView1.TabIndex = 6;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -136,36 +132,36 @@
             // columnHeader1
             // 
             this.columnHeader1.Text = "STT";
-            this.columnHeader1.Width = 50;
+            this.columnHeader1.Width = 67;
             // 
             // columnHeader2
             // 
             this.columnHeader2.Text = "Tên SP";
-            this.columnHeader2.Width = 64;
+            this.columnHeader2.Width = 103;
             // 
             // columnHeader3
             // 
             this.columnHeader3.Text = "Mã SP";
-            this.columnHeader3.Width = 80;
+            this.columnHeader3.Width = 101;
             // 
             // columnHeader4
             // 
             this.columnHeader4.Text = "Nhà SX";
-            this.columnHeader4.Width = 66;
+            this.columnHeader4.Width = 148;
             // 
             // columnHeader5
             // 
             this.columnHeader5.Text = "Số Lượng";
-            this.columnHeader5.Width = 71;
+            this.columnHeader5.Width = 126;
             // 
             // columnHeader6
             // 
             this.columnHeader6.Text = "Giá Bán";
-            this.columnHeader6.Width = 70;
+            this.columnHeader6.Width = 119;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(326, 270);
+            this.button2.Location = new System.Drawing.Point(507, 388);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(95, 23);
             this.button2.TabIndex = 7;
@@ -173,22 +169,54 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Timkiemsanpham
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(100, 70);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(422, 38);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Loại Tìm Kiếm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Từ Khóa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Thông Tin Sản Phẩm";
+            // 
+            // TimKiemSanPhamPL
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 303);
+            this.ClientSize = new System.Drawing.Size(738, 441);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.radioButton3);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Name = "Timkiemsanpham";
+            this.Name = "TimKiemSanPhamPL";
             this.Text = "Tìm Kiếm Sản Phẩm";
             this.Load += new System.EventHandler(this.Timkiemsanpham_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -210,5 +238,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ColumnHeader columnHeader5;
         private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
