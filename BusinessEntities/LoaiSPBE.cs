2a6a9a1a502a5a1b6e60ade59b00c6c73bb8a933
vtienuit <vtienuit@a3f09300-fa11-11de-8cdf-e97ad7d0f28a>
2010-01-09 15:21:03 +0000

    Chỉnh sửa 10h20 9/1

--- BusinessEntities/LoaiSPBE.cs
+++ BusinessEntities/LoaiSPBE.cs
@@ -3,6 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+/**-----------------------------------------------------------
+* Program : QLCHVLXD
+* Written by : tiendv
+* Email : tiendv.vn@gmail.com
+•	Created date: 07/01/2010 
+* Modified by:
+•	Modified date:
+•	Version: 1.0	
+•	Description: 
+----------------------------------------------------------*/
 namespace BusinessEntities
 {
     public class LoaiSPBE
