6a796e35987e909c73082239cde6040ce98bd2ab
vtienuit <vtienuit@a3f09300-fa11-11de-8cdf-e97ad7d0f28a>
2010-01-09 15:23:13 +0000

    Fom Thêm đơn vị tính

---
+++ trunk/qlchvlxd/ThemDonViTinh.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BusinessEntities;
+using BusinessLogicLayer;
+
+namespace qlchvlxd
+{
+    public partial class ThemDonViTinh : Form
+    {
+        private BusinessLogicLayer.LoaiDonViTinhBLL loaidonvitinhBLL = new BusinessLogicLayer.LoaiDonViTinhBLL();
+  
+        List<BusinessEntities.LoaiDonViTinhBE> listLoaiDonViTinh = new List<BusinessEntities.LoaiDonViTinhBE>();
+        public BusinessLogicLayer.LoaiDonViTinhBLL loaidonvitinh = new BusinessLogicLayer.LoaiDonViTinhBLL();
+      
+        public int madvt;
+        
+        public ThemDonViTinh()
+        {
+            InitializeComponent();
+        }
+
+        public void hienthi()
+        {
+            //  BusinessLogicLayer.SanPhamBLL sanphamBLL = new BusinessLogicLayer.SanPhamBLL();
+
+            listLoaiDonViTinh = BusinessLogicLayer.LoaiDonViTinhBLL.getDanhSachLoaiDonViTinh();
+            if (listLoaiDonViTinh == null)
+                MessageBox.Show("Chi tiết sản phẩm rỗng ");
+            else
+            {
+              
+                int stt = 1;
+                listView1.Items.Clear();
+                foreach ( LoaiDonViTinhBE dv in listLoaiDonViTinh)
+                {
+                    dv.MALOAIDONVITINH.ToString();
+                    ListViewItem item = new ListViewItem(stt.ToString());
+                    stt++;
+                    item.SubItems.Add(dv.MALOAIDONVITINH.ToString());
+                    item.SubItems.Add(dv.TENDONVITINH.ToString());
+                    listView1.Items.Add(item);
+                   
+                }
+                madvt = Int32.Parse(listView1.Items[stt - 2].SubItems[1].Text);
+                textBox1.Text = (madvt + 1).ToString();
+
+            }
+
+        }
+
+        private void ThemDonViTinh_Load(object sender, EventArgs e)
+        {
+            hienthi();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length == 0)
+                MessageBox.Show("Tên đơn vị tính không được bỏ trống");
+            else
+            {
+                LoaiDonViTinhBE ldvt = new LoaiDonViTinhBE();
+                ldvt.MALOAIDONVITINH = int.Parse(textBox1.Text);
+                ldvt.TENDONVITINH = textBox2.Text.ToUpper();
+                loaidonvitinhBLL.themdonvitinh(ldvt);
+                hienthi();
+                textBox2.Clear();
+            
+            }
+        }
+
+       
+
+      
+
+       
+    }
+}
